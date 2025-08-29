b41358ab68bcf5537e2fe62111ac168b55406ceb
RACHEL DAVEY <racheld9999@outlook.com>
2025-08-29 18:35:51 +0100

    Ready for heroku

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import django_heroku
+from decouple import config
+import dj_database_url
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
