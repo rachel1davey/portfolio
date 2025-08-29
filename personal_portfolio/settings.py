660c68422f4caea5a03dadc5b9e363a8fa158d89
Rachel Davey <rachel.davey@libfstudy.ac.uk>
2025-08-29 15:12:17 +0100

    Basic /projects page with dynamic content and database

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_ROOT = BASE_DIR / "uploads/"
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -32,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'projects.apps.ProjectsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
